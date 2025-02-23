23cef5001934e7a30291519d683b1007f0570c85
medericc <bleublanctour1@>
2025-02-23 11:34:00 +0100

    ee

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,39 @@
 import './globals.css';
+
+export const metadata = {
+    title: "Lucile & Carla Stats",
+    description: "Les stats détaillées en direct.",
+    icons: {
+        icon: "/favicon.ico", // Pour le favicon par défaut
+        shortcut: "/favicon.ico", // Pour les navigateurs type iOS
+        apple: "/apple-touch-icon.png", // iPhone/iPad
+    },
+    openGraph: {
+      title: "Lucile & Carla Stats",
+      description: "Les stats détaillées en direct.",
+      url: "https://carla-lucile.vercel.app/",
+      siteName: "Lucile & Carla Stats",
+      images: [
+        {
+          url: "https://carla-lucile.vercel.app/preview.jpg", // Mets une image propre ici !
+          width: 1200,
+          height: 630,
+          alt: "Site de Carla et Lucile",
+        },
+      ],
+      type: "website",
+    },
+    twitter: {
+      card: "summary_large_image", // ✅ Correction ici
+      title: "Lucile & Carla Stats",
+      description: "Les stats détaillées en direct.",
+      images: ["https://carla-lucile.vercel.app/preview.jpg"], // Même image que Open Graph
+    },
+  };
+  
+
+
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
       <html lang="fr">
