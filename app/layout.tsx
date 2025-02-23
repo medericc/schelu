2316f7380c942dda50d0de2a1636dc7c7de071d2
medericc <bleublanctour1@>
2025-02-23 11:07:06 +0100

    ee

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
       <html lang="fr">
           <body className="min-h-screen bg-gray-100 text-gray-900 dark:bg-gray-900 dark:text-white">
-          <header className="bg-gradient-to-r from-blue-800 to-blue-950 text-white p-8 text-4xl font-extrabold text-center shadow-md">
+          <header className="bg-gradient-to-r from-blue-900 to-blue-950 text-white p-8 text-4xl font-extrabold text-center shadow-md">
     LIVESTATS
 </header>
               <main className="container mx-auto mt-4">{children}</main>
