edcf24a0bf2902781f76b2ebcb59adf3156c5e6c
medericc <bleublanctour1@>
2025-05-02 07:16:45 +0200

    ee

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import './globals.css';
 
 export const metadata = {
     title: "Calendrier de MJ Musa",
-    description: "Les matchs détaillées en direct.",
+    description: "Le calendrier des matchs de Musa.",
     icons: {
         icon: "/favicon.ico", // Pour le favicon par défaut
         shortcut: "/favicon.ico", // Pour les navigateurs type iOS
@@ -10,15 +10,15 @@ export const metadata = {
     },
     openGraph: {
       title: "Calendrier de MJ Musa",
-      description: "Les matchs détaillées en direct.",
-      url: "https://carla-lucile.vercel.app/",
-      siteName: "Lucile & Carla Stats",
+      description: "Le calendrier des matchs de Musa.",
+      url: "mj-musa-schedule.vercel.app/",
+      siteName: "MJ Musa Schedule",
       images: [
         {
-          url: "https://carla-lucile.vercel.app/preview.jpg", // Mets une image propre ici !
+          url: "mj-musa-schedule.vercel.app/preview.jpg", // Mets une image propre ici !
           width: 1200,
           height: 630,
-          alt: "Site de Carla et Lucile",
+          alt: "MJ Musa Schedule",
         },
       ],
       type: "website",
@@ -26,8 +26,8 @@ export const metadata = {
     twitter: {
       card: "summary_large_image", // ✅ Correction ici
       title: "Calendrier de MJ Musa",
-      description: "Les matchs détaillées en direct.",
-      images: ["https://carla-lucile.vercel.app/preview.jpg"], // Même image que Open Graph
+      description: "Le calendrier des matchs de Musa.",
+      images: ["mj-musa-schedule.vercel.app/preview.jpg"], // Même image que Open Graph
     },
   };
   
